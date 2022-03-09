c94d15276cc14325d5dde4ec044f2e6b3c487e93
WMascot <danila33366@gmail.com>
2022-03-09 21:10:49 +0300

    Created enum for types of Auto

---
+++ Automobile/Utils/AutoEnumerator.cs
@@ -0,0 +1,12 @@
+﻿namespace Automobile.Utils
+{
+    /// <summary>
+    /// Types of Auto
+    /// </summary>
+    public enum EnumType
+    {
+        Car,
+        Lorry,
+        SportCar
+    }
+}
