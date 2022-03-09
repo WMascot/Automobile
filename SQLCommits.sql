e880c577624020800245a46a93b63d36f36b2cab
WMascot <96870027+WMascot@users.noreply.github.com>
2022-03-09 21:57:34 +0300

    Update SQLCommits.sql

--- SQLCommits.sql
+++ SQLCommits.sql
@@ -10,6 +10,9 @@ CREATE TABLE orders
   customer_id INTEGER REFERENCES Customers (customer_id)
 );
 
-SELECT orders.id, customers.name FORM orders
-JOIN customers ON orders.customer_id = customers.customer_id
-ORDER BY orders.order_id;
+SELECT name FROM customers WHERE name NOT IN 
+(
+  SELECT customers.name FORM customers
+  JOIN orders ON orders.customer_id = customers.customer_id
+  ORDER BY orders.order_id
+);
