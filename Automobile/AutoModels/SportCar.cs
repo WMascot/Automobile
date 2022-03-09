990f493fa03af891ea0f7ea3d53035b308472e01
WMascot <danila33366@gmail.com>
2022-03-09 21:27:40 +0300

    Created an inherited SportCar class

---
+++ Automobile/AutoModels/SportCar.cs
@@ -0,0 +1,18 @@
+﻿using Automobile.AutoModels.Base;
+using Automobile.Utils;
+
+namespace Automobile.AutoModels
+{
+    public class SportCar : BaseAuto
+    {
+        public override EnumType Type { get { return EnumType.SportCar; } }
+
+        public SportCar(double avgConsume, double liquidAmount) : base(avgConsume, liquidAmount)
+        {
+        }
+        protected override double DistanceReduction(double distance)
+        {
+            return base.DistanceReduction(distance);
+        }
+    }
+}
